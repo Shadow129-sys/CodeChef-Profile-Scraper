a5ca8588408cd7efe3721325eab1cec9d70969e0
Sayan Ghosh <69137771+Shadow129-sys@users.noreply.github.com>
2021-03-29 09:33:20 -0400

    comment added

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ import fileinput
 
 if __name__ == '__main__':
     nameList = []
+    
+    #for extracting the usernames from each line of the "input.txt"
     for names in fileinput.input(files="input.txt"):
         names = names.strip()
         nameList.append(names)
 
+    #clearing out the "output.txt" file 
     f = open('output.txt', 'w')
     f.write("")
     f.close()
+    
+    #sending the usernames one by one to extract information
     for username in nameList:
         pf.FindProfile(username)
